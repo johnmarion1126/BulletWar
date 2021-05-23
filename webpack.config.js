1ed464c9f4c81124e4562052711db25ad12d8409
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-05-23 11:03:10 -0700

    Add html webpack plugin and a css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Html Game',
+    }),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
