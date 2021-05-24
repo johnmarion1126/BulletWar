501020e9dd528a9c4ada6a2f23d23ff2a4439ff3
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-05-24 15:25:58 -0700

    Uninstall html-webpack-plugin from dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,11 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Html Game',
-    }),
-  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
   module: {
     rules: [
