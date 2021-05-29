3277210e2d85e9abee90261d44f823f0e52bc7b9
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-05-29 16:42:38 -0700

    Remove unnecessary code, Add server using express

--- README.md
+++
@@ -1,6 +0,0 @@
-# Run And Shoot
-A small game made with html, css, and javascript.
-
-## References
-* [Game Logic](https://scripterswar.com/tutorial/html5#)
-* [Server Side](https://scripterswar.com/tutorial/nodejs)
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,14 @@
 const path = require('path');
 
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
-
 module.exports = {
   mode: 'development',
-  entry: './src/app.js',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+      contentBase: path.resolve(__dirname, 'dist')
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  plugins: [new NodePolyfillPlugin()],
-  resolve: {
-    fallback: {
-      fs: false,
-      tls: false,
-      net: false,
-      path: false,
-      zlib: false,
-      http: false,
-      https: false,
-      stream: false,
-      crypto: false,
-      'crypto-browserify': require.resolve('crypto-browserify'),
-    },
   },
 };
