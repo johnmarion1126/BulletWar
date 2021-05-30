2222017e4a1a85d3de862153587dcd1888163fa6
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-05-30 11:25:32 -0700

    Add server code for updating position

--- server.js
+++ server.js
@@ -4,20 +4,50 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io')(server, {});
+const config = require('./webpack.config');
 
-const config = require('./webpack.config.js');
 const compiler = webpack(config);
 
 app.use(
   webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath,
-  })
+  }),
 );
 
 app.listen(3000, () => {
+  // eslint-disable-next-line no-console
   console.log('Example app listening on port 3000!\n');
 });
 
 app.get('/', (req, res) => {
-    res.sendFile('dist/index.html', { root: __dirname })
+  res.sendFile('dist/index.html', { root: __dirname });
 });
+
+const socketList = [];
+
+io.sockets.on('connection', (socket) => {
+  socket.id = Math.random();
+  socket.x = 0;
+  socket.y = 0;
+  socket.number = ` ${Math.floor(10 * Math.random())}`;
+  socketList[socket.id] = socket;
+});
+
+setInterval(() => {
+  const pack = [];
+  socketList.forEach((i) => {
+    const socket = socketList[i];
+    socket.x += 1;
+    socket.y += 1;
+    pack.push({
+      x: socket.x,
+      y: socket.y,
+      number: socket.number,
+    });
+  });
+  socketList.forEach((i) => {
+    const socket = socketList[i];
+    socket.emit('newPositions', pack);
+  });
+}, 1000 / 25);
